521e0b78d10b26cfe977651064b2b700f8b7728e
Blizii <72545889+Blizii@users.noreply.github.com>
2021-01-24 17:03:39 +0100

    Basic Upload

---
+++ php/includes/fonts.php
@@ -0,0 +1,12 @@
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Raleway:300,400,500'); /* Raleway Font */
+    @import url('https://fonts.googleapis.com/css?family=Open+Sans'); /* Open Sans Font */
+    @font-face {
+        font-family: 'Anurati';
+        src: url('../../assets/fonts/Anurati-Regular.otf');
+    }
+    @font-face {
+        font-family: 'Nasa';
+        src: url('../../assets/fonts/nasalization-rg.ttf');
+    }
+</style>
